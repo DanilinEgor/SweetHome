70bffb798aaaff780ef147cf700a6c062e2b3a7a
Egor Danilin <edanilin@techops.ru>
2016-12-22 01:39:18 +0300

    Updated Gradle, Kotlin version. Moved Kotlin files to /kotlin folder

--- app/src/main/kotlin/xyz/egor_d/sweethome/HomeActivity.kt
+++ app/src/main/kotlin/xyz/egor_d/sweethome/HomeActivity.kt
@@ -3,7 +3,7 @@ package xyz.egor_d.sweethome
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class HomeActivity : AppCompatActivity(){
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
