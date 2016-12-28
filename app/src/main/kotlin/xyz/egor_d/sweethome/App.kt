7d2cc23bc78275ad0e694012ef866afda6a46723
Egor Danilin <edanilin@techops.ru>
2016-12-28 03:07:41 +0300

    Added Crashlytics

--- app/src/main/kotlin/xyz/egor_d/sweethome/App.kt
+++ app/src/main/kotlin/xyz/egor_d/sweethome/App.kt
@@ -1,11 +1,14 @@
 package xyz.egor_d.sweethome
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 import timber.log.Timber
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        Fabric.with(this, Crashlytics())
         Timber.plant(Timber.DebugTree())
     }
 }
